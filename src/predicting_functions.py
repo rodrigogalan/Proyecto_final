588232ad57dd2502d516b1a713ce131ec424032d
rodrigogalan <rodrigogalan98@gmail.com>
2022-05-23 22:24:32 +0200

    Mejora del archivo de predicciones y de las funciones usadas

--- src/predicting_functions.py
+++ src/predicting_functions.py
@@ -3,104 +3,165 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-import h2o
-from h2o.automl import H2OAutoML
-
 from sklearn.model_selection import train_test_split
 
 from sklearn.metrics import roc_auc_score, roc_curve, auc
 
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.under_sampling import RandomUnderSampler
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
 from lightgbm import LGBMClassifier
 
 
-def prueba_modelo(modelo, X_train, y_train, X_test, y_test):
-    '''Función para entrenar y testear modelos de forma rápida'''
-
-    modelo.fit(X_train, y_train)
-
-    train_score=modelo.score(X_train, y_train)  
-    test_score=modelo.score(X_test, y_test)
-
-    print(modelo)
-    print('Train:', train_score)
-    print('Test:', test_score) 
-    print('\n')
 
 
-def export_modelo(modelo, X_train, y_train, X_test):
-
-    modelo.fit(X_train, y_train)
-
-    sample=pd.read_csv('./Data/sample.csv')
-    sample.price=modelo.predict(X_test)
-    sample.to_csv('./Data/sample.csv',index=False)
-
-
-def h2o_function(n_models, usecols ):
-    h2o.init()
-
-    train = pd.read_csv('./Data/train_clean.csv', usecols=usecols)
-    test = pd.read_csv('./Data/test_clean.csv', usecols=usecols)
-    train_price =pd.read_csv('./Data/train_clean.csv', usecols=["price"])
-
-    train = pd.concat([train, train_price], axis=1)
-
-    train_h2o = h2o.H2OFrame(train)
-    test_h2o = h2o.H2OFrame(test)
-
-    X=train_h2o.columns
-    y='price'
-    X.remove(y)
-
-    train_h2o[y] = train_h2o[y]
-
-    aml=H2OAutoML(max_models = (n_models))
-
-    aml.train(x=X, y=y, training_frame=train_h2o)
-
-    print(aml.leaderboard)
-
-    sample=pd.read_csv('./Data/sample.csv')
-
-    sample.price = aml.leader.predict(test_h2o).as_data_frame()
-
-    sample.to_csv('./Data/sample.csv',index=False)
-
-
-
-def roc_curve_plot(modelo, x_train, x_test, y_train, y_test):
+def train_test(train):
     '''
-    This function train a model and print the roc_curve
+    This function takes an array and divide it in a train part and in a test part
+
+    Parameters:
+    train (array): Array to be divided
+
+    Returns:
+    X_train (array): array with data in the train part
+    X_test (array): array with data in the test part
+    y_train (array): array with the solution in the train part
+    y_test (array): array with the solution in the test part
+    '''
+    X = train.drop(columns=['target'])
+    y=train.target
+    return train_test_split(X, y, test_size=0.2)
+
+
+def confusion_matrix_total(y_test, y_pred):
+    '''
+    This function takes two arrays, a predicction and the solution and returns a confusion matrix
+
+    Parameters:
+    y_test (array): array with the solution
+    y_pred (array): array with the prediction
+    '''
+    ax=sns.heatmap(confusion_matrix(y_test, y_pred)/sum(sum(confusion_matrix(y_test, y_pred))), annot=True)
+
+    plt.title('Matriz confusion')
+    plt.ylabel('Verdad')
+    plt.xlabel('Prediccion')
+    plt.show();
+
+def reg_model(modelo, x_train, x_test, y_train, y_test):
+    '''
+    This function trains a regresion model and returns the parameters needed to calculate the roc curve and the predictions
     
     Parameters:
     modelo (machine learning model): Modelo to be adjust and plot the roc curve
-
     x_train (array): Array to train the model 
-
     x_test (array): Array to test the model 
-
     y_train (array): Array to train the model (solution)
-
     y_test (array): Array to train the model (solution)
+
+    Returns:
+    fpr (array):   Increasing false positive rates 
+    tpr (array): Increasing true positive rates
+    roc_auc (float): area under the roc curve
     '''
     modelo.fit(X=x_train, y=y_train)
+    y_pred=modelo.predict(X=X_test)
+
     fpr, tpr, _ = roc_curve(y_test, modelo.predict_proba(x_test)[:, 1])
     roc_auc = auc(fpr, tpr)
+
+    return fpr, tpr, roc_auc, y_pred
+
+
+def roc_curve_plot(fpr, tpr, roc_auc):
+    '''
+    This function prints a roc curve
+    
+    Parameters:
+    fpr (array):   Increasing false positive rates 
+    tpr (array): Increasing true positive rates
+    roc_auc (float): area under the roc curve
+    '''
 
     plt.figure()
     plt.plot(fpr, tpr, color='red', label='ROC curve (area = %0.3f)' % roc_auc)
     plt.plot([0, 1], [0, 1], color='black', lw=2, linestyle='--')
     plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
+    plt.ylim([0.0, 1.0])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.title('ROC Curve')
     plt.legend(loc="upper left")
     plt.show()
+
+
+def export_modelo(y_pred):
+    '''
+    This funcition takes a solution array and exports it
+    '''
+
+    sample=pd.read_csv('./Data/sample.csv')
+    sample.price=y_pred
+    sample.to_csv('./Data/sample.csv',index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def function_prueba_datos(archivo):
